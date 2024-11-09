94b6b75703e7b0cbf9b2bed460f3a871e1e869ff
srvinn1 <sarvinozanvarova26@gmail.com>
2024-11-09 17:42:50 +0500

    Create test_sample.py

---
+++ test_sample.py
@@ -0,0 +1,10 @@
+
+def test_addition():
+    assert 1 + 1 == 2
+
+def test_string():
+    assert "hello".upper() == "HELLO"
+
+def test_list_length():
+    sample_list = [1, 2, 3]
+    assert len(sample_list) == 3
